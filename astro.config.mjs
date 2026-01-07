fb659708c1b176f3651bae50ec7a7f0a1c2db628
Andrej Bozic <abozic389@gmail.com>
2026-01-07 20:54:33 +0100

    fix: build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  image: { service: sharp() },
   vite: { plugins: [tailwindcss()] },
   integrations: [
     react(),
